72a5daa6d8b6060e88c2083d442f5f05992776eb
jinn2u <bsybear623@gmail.com>
2021-04-03 12:52:02 +0900

    pagination

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -1,9 +1,10 @@
 package com.cos.blog.controller;
 
-import com.cos.blog.config.auth.PrincipalDetail;
 import com.cos.blog.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,8 +16,8 @@ public class BoardController {
     private BoardService boardService;
 
     @GetMapping({"","/"})
-    public String index(Model model){
-        model.addAttribute("boards",boardService.writingList());
+    public String index(Model model, @PageableDefault(size = 2, sort = "id", direction = Sort.Direction.DESC) Pageable pabelable){
+        model.addAttribute("boards", boardService.writingList(pabelable));
         return "index";
     }
 

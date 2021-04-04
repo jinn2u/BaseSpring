0cb46442f4335e55414fc69117c1ff502b0bf92d
jinn2u <bsybear623@gmail.com>
2021-04-04 15:29:31 +0900

    session변경하기

--- src/main/java/com/cos/blog/service/UserService.java
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -4,6 +4,12 @@ import com.cos.blog.Repository.UserRepository;
 import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,5 +35,14 @@ public class UserService {
         user.setPassword(encPassword);
         user.setRole(RoleType.USER);
         userRepository.save(user);
+    }
+
+    @Transactional
+    public void modifyMember(User user){
+        User persistance = userRepository.findById(user.getId()).orElseThrow(()->{
+            return new IllegalArgumentException(("회원 찾기 실패"));
+        });
+        persistance.setPassword(encoder.encode(user.getPassword()));
+        persistance.setEmail(user.getEmail());
     }
 }
--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -2,6 +2,9 @@ let index = {
     init: function(){
         $('#btn-save').on('click', ()=> {  // this를 바인딩하기 위해서 화살표함수를 사용했다.
             this.save()
+        }),
+        $('#btn-update').on('click', ()=> {  // this를 바인딩하기 위해서 화살표함수를 사용했다.
+            this.update()
         })
     },
     save: function(){
@@ -19,6 +22,27 @@ let index = {
             dataType: "json"
         }).done(function(){
             alert("회원가입이 완료되었습니다.")
+            location.href = '/'
+        }).fail(function (error){
+            alert(JSON.stringify(error))
+        })// ajax 통신을 통하여 3개의 데이터를 json으로 변경하여 insert 요청
+    },
+    update: function(){
+        // alert("user의 save함수 호출됨")
+        let data = {
+            id: $("#id").val(),
+            username: $("#username").val(),
+            password: $("#password").val(),
+            email: $("#email").val()
+        }
+        $.ajax({
+            type: "PUT",
+            url: "/user",
+            data: JSON.stringify(data),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json"
+        }).done(function(){
+            alert("회원수정이 완료되었습니다.")
             location.href = '/'
         }).fail(function (error){
             alert(JSON.stringify(error))
